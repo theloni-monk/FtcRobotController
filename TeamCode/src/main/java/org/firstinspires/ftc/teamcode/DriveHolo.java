84f6716ec6158db580e9ab83c0eb545550de7b00
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-01-22 14:36:28 -0800

    reorganized code for oopiness

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveHolo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveHolo.java
@@ -33,7 +33,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorImplEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -59,8 +58,7 @@ public class DriveHolo extends LinearOpMode {
     private double MAX_RAD_PER_SEC = MAX_RPM / 60 / GEAR_RATIO * 2 * Math.PI;
     private double MAX_VEL_LIN  = MAX_RAD_PER_SEC * (WHEEL_RAD*WHEEL_RAD) ; //meters/sec  V = w * r^2 get linear vel by taking angular vel (rpm in rads) * r^2
 
-    @Override
-    public void runOpMode() {
+    public void initDriveOp(){
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -79,36 +77,45 @@ public class DriveHolo extends LinearOpMode {
         l1.setDirection(DcMotor.Direction.REVERSE);
         l2.setDirection(DcMotor.Direction.REVERSE);
 
+        runtime.reset();
+    }
+
+    public void executeDriveLogic(){
+        double triggerSpeed = gamepad1.right_trigger * MAX_RAD_PER_SEC; // master speed on trigger
+        if (gamepad1.b) triggerSpeed = 0; // press B to stop
+
+        // POV Mode uses left stick to go forward, and right stick to turn.
+        double turn = gamepad1.right_stick_x;
+        double drive = gamepad1.left_stick_y;
+
+
+        double leftAngVel = Range.clip(triggerSpeed * (drive + turn), -MAX_RAD_PER_SEC, MAX_RAD_PER_SEC);
+        double rightAngVel = Range.clip(triggerSpeed * (drive - turn), -MAX_RAD_PER_SEC, MAX_RAD_PER_SEC);
+
+        // Send calculated power to wheels
+        l1.setVelocity(leftAngVel, AngleUnit.RADIANS);
+        l2.setVelocity(leftAngVel, AngleUnit.RADIANS);
+        r1.setVelocity(rightAngVel, AngleUnit.RADIANS);
+        r2.setVelocity(rightAngVel, AngleUnit.RADIANS);
+
+
+        telemetry.addData("Instructions", "Left stick y for direction, right stick x for turning, right trigger for master scaling");
+        telemetry.addData("Status", "Run Time: " + runtime.toString() + " MotorController: " + r1.getController().toString());
+        // Shift speeds to linear speed when reporting
+        telemetry.addData("Motors", "Speeds: left (%.2f)m/s, right (%.2f)m/s", 2 * leftAngVel * (WHEEL_RAD * WHEEL_RAD), 2 * rightAngVel * (WHEEL_RAD * WHEEL_RAD));
+        telemetry.update();
+    }
+
+    @Override
+    public void runOpMode() {
+        initDriveOp();
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
-            double triggerSpeed = gamepad1.right_trigger * MAX_RAD_PER_SEC; // master speed on trigger
-            if(gamepad1.b) triggerSpeed = 0; // press B to stop
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            double turn  =  gamepad1.right_stick_x;
-            double drive = gamepad1.left_stick_y;
-
-
-            double leftAngVel    = Range.clip(triggerSpeed * (drive + turn), -MAX_RAD_PER_SEC, MAX_RAD_PER_SEC) ;
-            double rightAngVel   = Range.clip(triggerSpeed * (drive - turn), -MAX_RAD_PER_SEC, MAX_RAD_PER_SEC) ;
-
-            // Send calculated power to wheels
-            l1.setVelocity(leftAngVel, AngleUnit.RADIANS);
-            l2.setVelocity(leftAngVel, AngleUnit.RADIANS);
-            r1.setVelocity(rightAngVel, AngleUnit.RADIANS);
-            r2.setVelocity(rightAngVel, AngleUnit.RADIANS);
-
-
-            telemetry.addData("Instructions", "Left stick y for direction, right stick x for turning, right trigger for master scaling");
-            telemetry.addData("Status", "Run Time: " + runtime.toString() + " MotorController: " + r1.getController().toString());
-            // Shift speeds to linear speed when reporting
-            telemetry.addData("Motors", "Speeds: left (%.2f)m/s, right (%.2f)m/s", 2 * leftAngVel * (WHEEL_RAD*WHEEL_RAD), 2 * rightAngVel * (WHEEL_RAD*WHEEL_RAD));
-            telemetry.update();
+            executeDriveLogic();
         }
     }
 }
