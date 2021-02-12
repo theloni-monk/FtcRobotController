fc2c317662886a021fe95f536c4b62462abbd1a8
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-02-12 12:40:08 -0800

    wrote autonomous script to deliver wobble goal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeliverWobbleOp.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.lang.reflect.Member;
+
+@Autonomous(name = "Wobble Delivery", group = "Linear Opmode")
+public class DeliverWobbleOp extends AutoBot{
+    @Override
+    public void runOpMode(){
+        boolean runOnce = true;
+        initAutoBot();
+        composeTelemetry();
+        waitForStart();
+        while(opModeIsActive() && runOnce){
+            driveDist(1.4, DistanceUnit.METER);
+            telemetry.update();
+            sleep(5000);
+
+            rotate(90, 0.7);
+            telemetry.update();
+            sleep(5000);
+
+            rotate(-90,0.7);
+            telemetry.update();
+            sleep(5000);
+
+            driveDist(-1.4, DistanceUnit.METER);
+            telemetry.update();
+            runOnce = false;
+        }
+
+    }
+}
