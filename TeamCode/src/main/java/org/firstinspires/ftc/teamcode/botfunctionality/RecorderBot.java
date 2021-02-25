28eec2d4247d763ac0a8233e2ece9d799cf1b0a7
Theo <13920881+theloni-monk@users.noreply.github.com>
2021-02-25 15:05:56 -0800

    encountered mem error in serialization of map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botfunctionality/RecorderBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botfunctionality/RecorderBot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.botfunctionality;
 
+import android.util.Size;
+
 import com.google.gson.Gson;
 import com.qualcomm.robotcore.util.RobotLog;
 
@@ -23,7 +25,6 @@ public abstract class RecorderBot extends HolonomicDriveBot{
     public void initMap(){
         outputMap = new TreeMap<>();
         gson = new Gson();
-
     }
 
     public void initRecorderBot(){
@@ -35,17 +36,22 @@ public abstract class RecorderBot extends HolonomicDriveBot{
         if(initTime == 0) { initTime = System.currentTimeMillis();}
         if(prevVelL != this.leftAngVel || prevVelR != this.rightAngVel){
             long currTime = System.currentTimeMillis() - initTime;
-            RobotLog.vv("recording: ", currTime+": "+this.leftAngVel+","+ this.rightAngVel);
+            RobotLog.vv("Currently recording: ", currTime+": "+this.leftAngVel+","+ this.rightAngVel+"; TreeMap keys len: " +  outputMap.keySet().size());
+            RobotLog.vv("Free memory remaining", String.valueOf(Runtime.getRuntime().freeMemory()));
             outputMap.put(currTime, this.leftAngVel+","+ this.rightAngVel);
-            RobotLog.vv("growing map: ", outputMap.toString());
+            this.prevVelL = this.leftAngVel;
+            this.prevVelR = this.rightAngVel;
+            //RobotLog.vv("growing map: ", outputMap.toString());
         }
     }
 
     protected void saveFile(){
             outputMap.put(System.currentTimeMillis() - initTime, "0,0");
             RobotLog.vv("Map",outputMap.toString());
+            System.out.flush();
             RobotLog.vv("s", "\n\n\n");
             RobotLog.vv("Wobble", gson.toJson(outputMap));
+            System.out.flush();
             RobotLog.vv("s","\n\n\n");
 
     }
